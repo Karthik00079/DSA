1557bb4a8e6c743a9511786fd5accb5f6cdeaae8
Karthik.S <118165911+Karthik00079@users.noreply.github.com>
2023-07-18 08:50:03 +0530

    Create preorder.c

---
+++ preorder.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+   int data;
+   struct node *leftChild;
+   struct node *rightChild;
+};
+struct node *root = NULL;
+void insert(int data){
+   struct node *tempNode = (struct node*) malloc(sizeof(struct node));
+   struct node *current;
+   struct node *parent;
+   tempNode->data = data;
+   tempNode->leftChild = NULL;
+   tempNode->rightChild = NULL;
+
+   if(root == NULL) {
+      root = tempNode;
+   } else {
+      current = root;
+      parent = NULL;
+      while(1) {
+         parent = current;
+         
+         if(data < parent->data) {
+            current = current->leftChild;
+            
+            if(current == NULL) {
+               parent->leftChild = tempNode;
+               return;
+            }
+         }
+         else {
+            current = current->rightChild;
+
+            
+            if(current == NULL) {
+               parent->rightChild = tempNode;
+               return;
+            }
+         }
+      }
+   }
+}
+void pre_order_traversal(struct node* root){
+   if(root != NULL) {
+      printf("%d ",root->data);
+      pre_order_traversal(root->leftChild);
+      pre_order_traversal(root->rightChild);
+   }
+}
+int main(){
+   int i;
+   int array[7] = { 27, 14, 35, 10, 19, 31, 42 };
+   for(i = 0; i < 7; i++)
+      insert(array[i]);
+   printf("\nPreorder traversal: ");
+   pre_order_traversal(root);
+   return 0;
+}
